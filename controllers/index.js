40b8cf64e3a86a4a6eb44348c350c466e75a7c4a
TKShadowBlade <katanablack85@gmail.com>
2021-12-27 01:32:02 -0500

    file creation

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homepageRoutes = require('./api/homepage-routes');
+const dashRoutes = require('./api/dash-routes');
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
