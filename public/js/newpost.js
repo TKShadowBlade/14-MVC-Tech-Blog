82cb8fe92e9b922828bf1a10d3e63dbc3e58031d
TKShadowBlade <katanablack85@gmail.com>
2021-12-30 00:55:55 -0500

    login logout and registration

---
+++ public/js/newpost.js
@@ -0,0 +1,19 @@
+const newFormHandler = async function(evt) {
+    evt.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
+
+    await fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            body,
+        }),
+        headers: { 'Content-Type': 'application/json '},
+    });
+
+    document.location.replace('/dash');
+};
+
+document.querySelector('#new-post').addEventListener('submit', newFormHandler);
---
+++ public/js/register.js
@@ -0,0 +1,24 @@
+const registerFormHandler = async function(evt) {
+    evt.preventDefault();
+
+    const usernameEl = document.querySelector('#username-input-register');
+    const passwordEl = document.querySelector('#password-input-register');
+
+    const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dash');
+    }
+    else {
+        alert('Registration Failed')
+    }
+};
+
+document.querySelector('#reg-form').addEventListener('submit', registerFormHandler);
