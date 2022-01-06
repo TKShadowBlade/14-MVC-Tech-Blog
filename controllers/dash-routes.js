c05dfb719bba4113b12a16b1fe55cd2c7b783384
TKShadowBlade <katanablack85@gmail.com>
2022-01-06 17:13:26 -0500

    updating dash-routes and admin-posts handlebars

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -12,6 +12,38 @@ router.get('/', withAuth, async (req, res) => {
 
         const posts = postData.map((post) => post.get({ plain: true }));
 
-        res.render
+        res.render('admin-posts', {
+            layout: 'dash',
+            posts,
+        });
+    } catch(err) {
+        res.redirect('login');
     }
-})
+});
+
+router.get('/new', withAuth, (req, res) => {
+    res.render('new-post', {
+        layout: 'dash',
+    });
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Posting.findByPk(req.params.id);
+
+        if(postData) {
+            const post = postData.get({plain: true});
+
+            res.render('edit', {
+                layout: 'dash',
+                post,
+            });
+        } else {
+            res.status(404).end();
+        }
+    }   catch(err) {
+        res.redirect('login');
+    }
+});
+
+module.exports = router;
